5b2a83e13842c185527ec26585d44d222a7ddb37
Marcos Santos <marcossamuel1710@outlook.com>
2024-07-26 12:03:48 -0300

    fix: correção na api gateway

---
+++ hr-apigateway/src/main/java/com/msdev/hr_apigateway/HrApigatewayApplication.java
@@ -0,0 +1,13 @@
+package com.msdev.hr_apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrApigatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrApigatewayApplication.class, args);
+	}
+
+}
